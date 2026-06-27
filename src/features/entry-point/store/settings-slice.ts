628d4603ef30f87e1fd665927d64dbf656cde505
Netoperer <netoperer@users.noreply.github.com>
2026-06-27 21:21:47 +0200

    feat: add setttings slice

---
+++ src/features/entry-point/store/settings-slice.ts
@@ -0,0 +1,21 @@
+import type { StateCreator } from "zustand";
+import type { StoreState } from ".";
+
+export interface SettingsSlice {
+  showNodeIds: boolean;
+  setShowNodeIds: (value: boolean) => void;
+}
+
+export const createSettingsSlice: StateCreator<
+  StoreState,
+  [["zustand/immer", never]],
+  [],
+  SettingsSlice
+> = (set): SettingsSlice => ({
+  showNodeIds: false,
+
+  setShowNodeIds: (value) =>
+    set((state) => {
+      state.showNodeIds = value;
+    }),
+});
