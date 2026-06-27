8709c6e03a9a016329149a68ba98dd743c2413c6
Netoperer <netoperer@users.noreply.github.com>
2026-06-27 21:23:47 +0200

    feat: add debug settings

--- src/features/entry-point/store/index.ts
+++ src/features/entry-point/store/index.ts
@@ -6,14 +6,18 @@ import {
 import { immer } from "zustand/middleware/immer";
 import { createSelectionSlice, type SelectionSlice } from "./selection-slice";
 import { createShareSlice, type ShareSlice } from "./share-slice";
+import { createBuilderSlice, type BuilderSlice } from "./builder-slice";
+import { createSettingsSlice, type SettingsSlice } from "./settings-slice";
 
-export type StoreState = PersistentSlice & SelectionSlice & ShareSlice;
+export type StoreState = PersistentSlice & SelectionSlice & ShareSlice & BuilderSlice & SettingsSlice;
 
 export const useEntryPointStore = create<StoreState>()(
   immer((...args) => ({
     ...createPersistentSlice(...args),
     ...createSelectionSlice(...args),
     ...createShareSlice(...args),
+    ...createBuilderSlice(...args),
+    ...createSettingsSlice(...args),
   })),
 );
 
--- src/features/entry-point/store/selectors/index.ts
+++ src/features/entry-point/store/selectors/index.ts
@@ -1,3 +1,2 @@
 export * from "./select-perks";
 export * from "./select-selected-nodes";
-
